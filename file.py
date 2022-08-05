996deb91caac18c5897685190ee2804d2ed50a36
nickypooz <31872123+nickypooz@users.noreply.github.com>
2022-08-05 15:40:56 -0700

    Create file.py

---
+++ file.py
@@ -0,0 +1,26 @@
+"""
+def hi(n1 , n2):
+    if n1 * n2 <= 1000:
+        return n1 * n2
+    else:
+        return n1 + n2
+print(hi(3,4))
+print (hi(1000,2))
+"""
+
+# def average (n):
+#     sum = 0
+#     for num in range(n + 1):
+#         sum = sum + num
+#     return sum / n
+# print(average(5))
+
+# def hello(n):
+#     if n % 2 > 0:
+#         return("odd")
+#     else:
+#         return("even")
+
+# for num in range(20):
+#     print(hello(num))
+
